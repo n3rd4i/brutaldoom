0ddc111cf938c8f33fd137a76a1e1350f694ff20
Andrei H <n3rd4i@gmail.com>
2019-05-30 11:35:31 +0200

    Initial commit

---
+++ ReadMe.md
@@ -0,0 +1,13 @@
+﻿# Zandronum install
+## Build package
+Brutal Doom download Website (checksum visibile in this link):
+1. Go to download website: https://www.moddb.com/downloads/start/95667/all
+2. Right+Click on any mirror and copy hyperlink
+3. Set url variable with new copied hyperlink: `url='LINK_HERE'`
+4. Download the asset: `wget -nc -O tools/assets/brutalv21.rar $url`
+5. Calculate MD5 Hash: %chocolateyinstall%\tools\checksum tools\assets\brutalv21.rar
+6. Check MD5 Hash: a42f7a1f4fbec5b19591ece7f9811034
+
+## References
+* Download [link](https://www.moddb.com/downloads/start/95667/all)
+* [BrutalDoom](https://www.moddb.com/mods/brutal-doom/downloads/brutal-doom-v21-beta)
---
+++ tools/chocolateyuninstall.ps1
@@ -0,0 +1,29 @@
+﻿$ErrorActionPreference = 'Stop'; # stop on all errors
+$installLocation = "$ENV:LOCALAPPDATA\Programs\BrutalDoom\*"
+$iconPath = "$ENV:UserProfile\Desktop\BrutalDoom.lnk"
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  softwareName  = 'brutaldoom*'  #part or all of the Display Name as you see it in Programs and Features. It should be enough to be unique
+}
+$uninstalled = $false
+[array]$key = Get-UninstallRegistryKey -SoftwareName $packageArgs['softwareName']
+if ($key.Count -eq 1) {
+  $key | % { 
+    $packageArgs['file'] = "$($_.UninstallString)" #NOTE: You may need to split this if it contains spaces, see below
+    if ($packageArgs['fileType'] -eq 'MSI') {
+      $packageArgs['silentArgs'] = "$($_.PSChildName) $($packageArgs['silentArgs'])"
+      $packageArgs['file'] = ''
+    } else {
+    }
+    Uninstall-ChocolateyPackage @packageArgs
+  }
+} elseif ($key.Count -eq 0) {
+  Write-Warning "$packageName has already been uninstalled by other means."
+} elseif ($key.Count -gt 1) {
+  Write-Warning "$($key.Count) matches found!"
+  Write-Warning "To prevent accidental data loss, no programs will be uninstalled."
+  Write-Warning "Please alert package maintainer the following keys were matched:"
+  $key | % {Write-Warning "- $($_.DisplayName)"}
+}
+Remove-Item $installLocation -exclude *.ini, *.zds -recurse -force
+Remove-Item $iconPath -force
