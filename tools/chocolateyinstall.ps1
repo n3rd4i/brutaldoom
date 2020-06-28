087200c18a77f0bee816ac4cb1eb5627e7cfa1e2
Andrei Hutanu <andrei.hutanu@evalan.com>
2020-06-28 16:21:27 +0200

    refactored version

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -3,43 +3,38 @@ $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 . "$toolsDir\commonEnv.ps1"
 . "$toolsDir\dependenciesEnv.ps1"
 
-$srcURL = 'https://www.moddb.com/downloads/start/95667'
-$tokenURL = "($($srcURL.replace('start','mirror'))/\w+/\w+)"
-$request = [System.Net.WebRequest]::Create($srcURL)
-$response = $request.GetResponse().GetResponseStream()
-$content = (New-Object System.IO.StreamReader $response).ReadToEnd()
-$url = (select-string -Input $content -Pattern $tokenURL).Matches[0].Value
-
-$BDModPack = "brutalv21.pk3"
+$url = Get-ModdbDlUrl 'https://www.moddb.com/downloads/start/95667'
 $brutalDoomArgs = @{
   packageName   = 'BrutalDoomMod'
   url           = $url
   unzipLocation = $installLocation
-  softwareName  = 'brutal*'
-  checksum      = 'A42F7A1F4FBEC5B19591ECE7F9811034'
-  checksumType  = 'md5'
+  checksum      = '088C35D934B0CE29715B0CA8F4C67BC82EFCE7DA8458D42337A026E7F5E68D19'
+  checksumType  = 'sha256'
 }
 Install-ChocolateyZipPackage @brutalDoomArgs
 
-Install-ChocolateyShortcut -ShortcutFilePath "$lnkDesktop" `
-  -TargetPath "$zandronumLocation\zandronum.exe" `
-  -Arguments "$installLocation\$BDModPack" `
-  -IconLocation "$toolsDir\assets\brutal_doom.ico" `
-  -WorkingDirectory "$installLocation"
-Install-ChocolateyShortcut -ShortcutFilePath "$startMenuDir\BrutalDoom.lnk" `
-  -TargetPath "$zandronumLocation\zandronum.exe" `
-  -Arguments "$installLocation\$BDModPack" `
-  -IconLocation "$toolsDir\assets\brutal_doom.ico" `
-  -WorkingDirectory "$installLocation"
+# Cleanup link first (safely)
+& $ENV:COMSPEC /C IF EXIST $pk3_lnk DEL /S /Q $pk3_lnk
 
-Install-ChocolateyShortcut -ShortcutFilePath "$startMenuDir\BrutalDoom Manual.lnk" `
-  -TargetPath "$installLocation\BRUTAL DOOM MANUAL.rtf" `
+# Install symbolic link for later use
+$pk3_src = "$(Join-Path $installLocation $BDModPack)"
+& $ENV:COMSPEC /C MKLINK $pk3_lnk $pk3_src
 
-Install-ChocolateyShortcut -ShortcutFilePath "$startMenuDir\BD startServer [LAN].lnk" `
+# StartMenu
+Install-ChocolateyShortcut "$startMenuDir\BD startServer [LAN].lnk" `
   -TargetPath "$zandronumLocation\zandronum.exe" `
   -Arguments "$installLocation\$BDModPack -iwad freedoom1.wad -port 10666 -host " `
   -WorkingDirectory "$installLocation"
-Install-ChocolateyShortcut -ShortcutFilePath "$startMenuDir\BD joinLocal [LAN].lnk" `
+Install-ChocolateyShortcut "$startMenuDir\BD joinLocal [LAN].lnk" `
   -TargetPath "$zandronumLocation\zandronum.exe" `
   -Arguments "$installLocation\$BDModPack -iwad freedoom1.wad -connect 127.0.0.1:10666" `
+  -WorkingDirectory "$installLocation"
+
+Install-ChocolateyShortcut "$startMenuDir\BrutalDoom Manual.lnk" `
+  -TargetPath "$installLocation\BRUTAL DOOM MANUAL.rtf" `
+
+Install-ChocolateyShortcut "$startMenuDir\BrutalDoom.lnk" `
+  -TargetPath "$zandronumLocation\zandronum.exe" `
+  -Arguments "$installLocation\$BDModPack" `
+  -IconLocation "$toolsDir\assets\brutal_doom.ico" `
   -WorkingDirectory "$installLocation"
--- tools/commonEnv.ps1
+++ tools/commonEnv.ps1
@@ -1,3 +1,6 @@
 $installLocation = "$ENV:LocalAppData\Programs\BrutalDoom"
 $lnkDesktop = "$ENV:UserProfile\Desktop\BrutalDoom.lnk"
 $startMenuDir = "$ENV:AppData\Microsoft\Windows\Start Menu\Programs\BrutalDoom"
+
+$BDModPack = "brutalv21.pk3"
+$pk3_lnk = "$(Join-Path $ENV:DOOMWADDIR $BDModPack)"
